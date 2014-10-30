390e1359015cc8b94a5863cd724c7198fac74e0e
Matthew Renaud <mrenaud@google.com>
2014-10-30 15:22:45 -0700

    Relax monad constraints on logMsg utility.

--- Firewall/Logging.hs
+++ Firewall/Logging.hs
@@ -11,11 +11,12 @@ module Firewall.Logging where
 import Firewall.Common
 
 import Control.Monad (when)
-import Control.Monad.RWS (ask, tell)
+import Control.Monad.RWS (MonadReader, MonadWriter, ask, tell)
 
 -- | Write a message with the specified to the logs, but only if logging is enabled in the configs.
 -- Logging is enabled if logLevel > 0.
-logMsg :: LogLevel -> String -> FirewallMonad ()
+-- Works within any monad that can read the Config and write to logs.
+logMsg :: (MonadReader Config m, MonadWriter [LogEntry] m) => LogLevel -> String -> m ()
 logMsg level s = do
   config <- ask
   when (getLogLevel config > 0) $ tell [LogEntry level s]
